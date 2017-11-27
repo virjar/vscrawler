95a7500c1957ce6a182659c4e7ac3ec3e8d220c5
virjar <weijia.deng@qunar.com>
2017-11-27 17:28:07 +0800

    proxy adapter

---
+++ vscrawler-core/src/main/java/com/virjar/vscrawler/core/net/proxy/strategy/ProxyAdapter.java
@@ -0,0 +1,52 @@
+package com.virjar.vscrawler.core.net.proxy.strategy;
+
+import com.virjar.vscrawler.core.net.proxy.Proxy;
+import org.apache.http.Header;
+
+import java.util.List;
+
+/**
+ * Created by virjar on 2017/11/27.<br/>
+ * 大多数请求代理只需要ip&port
+ */
+public abstract class ProxyAdapter implements Proxy {
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public List<Header> getAuthenticationHeaders() {
+        return null;
+    }
+
+    @Override
+    public void offline() {
+
+    }
+
+    @Override
+    public void block(long blockTimeStamp) {
+
+    }
+
+    @Override
+    public void recordUsage() {
+
+    }
+
+    @Override
+    public void recordFailed() {
+
+    }
+
+    @Override
+    public boolean isDisable() {
+        return false;
+    }
+}
