17410759664b6ce6ae319f05bce5a09b7343f6f5
virjar <weijia.deng@qunar.com>
2017-11-28 13:27:27 +0800

    0.1.1

--- vscrawler-core/src/main/java/com/virjar/vscrawler/core/VSCrawler.java
+++ vscrawler-core/src/main/java/com/virjar/vscrawler/core/VSCrawler.java
@@ -341,7 +341,7 @@ public class VSCrawler extends Thread implements CrawlerConfigChangeEvent, First
         synchronized (System.out) {// 血可流头可断,队形不能乱
             System.err.println("################################################");
             System.err.println("##############     VSCrawler      ##############");
-            System.err.println("##############       0.0.1        ##############");
+            System.err.println("##############       0.1.1        ##############");
             System.err.println("############## 你有一个有意思的灵魂 ##############");
             System.err.println("################################################");
             System.err.println("##############       virjar       ##############");
