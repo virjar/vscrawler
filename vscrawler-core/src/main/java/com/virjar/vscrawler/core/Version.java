4f7030056add757c2aab7619453c0f6837d3d10f
virjar <weijia.deng@qunar.com>
2018-02-01 10:20:37 +0800

    aoto scan pom.properties for version parser

--- vscrawler-core/src/main/java/com/virjar/vscrawler/core/Version.java
+++ vscrawler-core/src/main/java/com/virjar/vscrawler/core/Version.java
@@ -3,9 +3,16 @@ package com.virjar.vscrawler.core;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.security.CodeSource;
+import java.util.Enumeration;
 import java.util.Properties;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * Created by virjar on 2018/1/18.<br>
@@ -13,7 +20,7 @@ import java.util.Properties;
 @Slf4j
 class Version {
     private static boolean printBanner = false;
-    private static String version = "0.2.x";
+    private static String version = "unknown";
     private static String groupId = "com.virjar";
     private static String artifactId = "vscrawler-core";
 
@@ -42,20 +49,113 @@ class Version {
     }
 
     private static void judgeVersion() {
+        if (!loadWithClassLoader()) {
+            loadWithPomPath();
+        }
+    }
+
+    private static boolean loadWithClassLoader() {
+        CodeSource codeSource = Version.class.getProtectionDomain().getCodeSource();
+        if (codeSource == null) {
+            return false;
+        }
+        //find pom.properties
+        URL location = codeSource.getLocation();
+        JarFile jarFile = null;
+        try {
+            if (location.toString().startsWith("file:")) {
+                File file = new File(location.getPath());
+                if (!file.exists()) {
+                    return false;
+                } else if (file.isDirectory()) {
+                    return loadWithDirector(file);
+                }
+                jarFile = new JarFile(new File(location.getPath()));
+            } else {
+                Object content = location.getContent();
+                if (content instanceof JarFile) {
+                    jarFile = (JarFile) content;
+                }
+            }
+            if (jarFile == null) {
+                return false;
+            }
+            Enumeration<JarEntry> entries = jarFile.entries();
+            while (entries.hasMoreElements()) {
+                JarEntry jarEntry = entries.nextElement();
+                String entryName = jarEntry.getName();
+                if (!jarEntry.isDirectory() && entryName.endsWith("pom.properties")) {
+                    InputStream jarFileInputStream = jarFile.getInputStream(jarEntry);
+                    try {
+                        loadMavenProperties(jarFileInputStream);
+                    } finally {
+                        IOUtils.closeQuietly(jarFileInputStream);
+                    }
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            log.error("error when open file pom.properties", e);
+        } finally {
+            IOUtils.closeQuietly(jarFile);
+        }
+        return false;
+    }
+
+    private static boolean loadWithDirector(File root) {
+        File pomPropertiesFile = findPomPropertiesFile(root);
+        if (pomPropertiesFile == null) {
+            return false;
+        }
+
+        InputStream pomPropertiesStream = null;
+        try {
+            pomPropertiesStream = new FileInputStream(pomPropertiesFile);
+            loadMavenProperties(pomPropertiesStream);
+            return true;
+        } catch (IOException e) {
+            log.error("error when open file pom.properties", e);
+        } finally {
+            IOUtils.closeQuietly(pomPropertiesStream);
+        }
+        return false;
+    }
+
+
+    private static File findPomPropertiesFile(File dir) {
+        File files[] = dir.listFiles();
+        if (null == files || files.length == 0)
+            return null;
+        for (File file : files) {
+            if (file.isDirectory()) {
+                return findPomPropertiesFile(file);
+            } else if (file.getName().endsWith("pom.properties")) {
+                return file;
+            }
+        }
+        return null;
+    }
+
+    private static void loadWithPomPath() {
         InputStream pomProperties = Version.class.getResourceAsStream("/META-INF/maven/com/virjar/vscrawler-core/pom.properties");
         if (pomProperties == null) {
             return;
         }
         try {
-            Properties properties = new Properties();
-            properties.load(pomProperties);
-            version = properties.getProperty("version", "0.2.x");
-            groupId = properties.getProperty("groupId", "com.virjar");
-            artifactId = properties.getProperty("artifactId", "vscrawler-core");
-        } catch (IOException e) {
-            log.error("error when load pom.properties", e);
+            loadMavenProperties(pomProperties);
+        } catch (IOException ioe) {
+            log.error("error when open file pom.properties", ioe);
         } finally {
             IOUtils.closeQuietly(pomProperties);
         }
     }
+
+    private static void loadMavenProperties(InputStream inputStream) throws IOException {
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        version = properties.getProperty("version", "unknown");
+        groupId = properties.getProperty("groupId", "com.virjar");
+        artifactId = properties.getProperty("artifactId", "vscrawler-core");
+    }
+
 }
