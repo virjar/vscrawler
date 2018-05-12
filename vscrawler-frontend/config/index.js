ec9880ba0f44a90cc83c1eb4b156b5940a317e7b
virjar <weijia.deng@qunar.com>
2018-05-12 14:46:32 +0800

    move fronend project

--- vscrawler-frontend/config/index.js
+++ vscrawler-frontend/config/index.js
@@ -48,10 +48,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../vscrawler-web/src/main/webapp/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../vscrawler-web/src/main/webapp'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
